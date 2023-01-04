43d61aa59937e0e2fbe8bec0c13bb272d83cd980
andrewlemay <56843864+andrewlemay@users.noreply.github.com>
2023-01-03 19:35:37 -0500

    Added documentation to TAPP2

--- TAPP2_CountdownTimer.py
+++ TAPP2_CountdownTimer.py
@@ -4,6 +4,7 @@
 
 from datetime import date, datetime, timedelta, time
 from dateutil.relativedelta import relativedelta
+# Loops until a date in the future is given
 invalidDate = True
 while invalidDate:
     print("Please enter a year, month, and day in the future that you would like to get a countdown to.")
@@ -11,15 +12,18 @@ while invalidDate:
     month = int(input("Month (MM): "))
     day = int(input("Day (DD): "))
 
+    # Creates a date object of the input date
     futureDate = date(year, month, day)
+    # Get the number of days between the future date and today
     delta = futureDate - date.today()
     numDays = delta.days
+    # Only calculates if the date is in the future
     if numDays >= 0:
         invalidDate = False
-        print(numDays)
+        # Gets the seconds until midnight
         seconds = datetime.combine(datetime.now() + timedelta(days=numDays), time())
         seconds = int((seconds - datetime.now()).seconds)
-        print(seconds)
+        # Converts the seconds into hours, minutes, and seconds
         hours = seconds//3600
         seconds -= hours*3600
         minutes = seconds//60
