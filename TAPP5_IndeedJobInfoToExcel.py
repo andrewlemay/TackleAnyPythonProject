a47fd2f99f1ecacb695cae0ab458c8262811072f
andrewlemay <56843864+andrewlemay@users.noreply.github.com>
2023-02-23 13:26:28 -0500

    Add the ability for TAPP5 to take a text file with multiple links

--- TAPP5_IndeedJobInfoToExcel.py
+++ TAPP5_IndeedJobInfoToExcel.py
@@ -10,46 +10,67 @@ import requests # used for making the call to the Indeed API
 from openpyxl import load_workbook # used for writing to the excel spreadsheet
 import datetime # used for getting the current date
 
-# enter the name of your workbook here, you could also ask for input but it would be tedious
-WB_NAME = 'YOUR_WORKBOOK_NAME.xlsx'
+"""Enter your workbook name and API key here. I have chosen to keep both of them hardcoded
+but workbook name could be put into main and entered through input if you want to separate
+workbooks for different job searches"""
+WB_NAME = "YOUR_WORKBOOK_NAME.xlsx"
 API_KEY = "YOUR_API_KEY"
 
-job_url = input('Enter the url of the job posting: ') # get the url of the job posting
-job_id = job_url[job_url.index('jk=')+3:job_url.index('&vjs')] # get the job id from the url
+def main():
+    multiple = input('Are there multiple jobs you want to add? (y/n): ')
+    if multiple.lower()[:1] == 'y': # enter if there are multiple jobs to add
+        job_doc = input('Enter the name of the file with the job links: ')
+        with open(job_doc) as job_links: # open the file with the job links
+            for link in job_links: # loop through the links
+                get_job_info(link) # call the function to get the job info from the Indeed API
+    else:
+        job_url = input('Enter the url of the job posting: ') # get the url of the job posting
+        get_job_info(job_url) # call the function to get the job info from the Indeed API
+    
 
-url = "https://indeed12.p.rapidapi.com/job/"+job_id # create the url for the Indeed API call
+def get_job_info(job_url): # function to get the job info from the Indeed API
+    job_id = job_url[job_url.index('jk=')+3:job_url.index('&vjs')] # get the job id from the url
 
-headers = {
-	"X-RapidAPI-Key": API_KEY,
-	"X-RapidAPI-Host": "indeed12.p.rapidapi.com"
-}
+    url = "https://indeed12.p.rapidapi.com/job/"+job_id # create the url for the Indeed API call
 
-response = requests.request("GET", url, headers=headers) # make the call to the Indeed API
+    headers = {
+        "X-RapidAPI-Key": API_KEY,
+        "X-RapidAPI-Host": "indeed12.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers) # make the call to the Indeed API
 
-posting_dict = response.json() # convert the response to a dictionary
+    posting_dict = response.json() # convert the response to a dictionary
 
-job_title = posting_dict['job_title'] # get the job title
+    job_title = posting_dict['job_title'] # get the job title
 
-company = posting_dict['company']['name'] # get the company name
+    company = posting_dict['company']['name'] # get the company name
 
-location_dirty = posting_dict['location'] # get the location
-location = ''.join([i for i in location_dirty if not i.isdigit()]) # remove the zip code from the location if it exists
+    location_dirty = posting_dict['location'] # get the location
+    location = ''.join([i for i in location_dirty if not i.isdigit()]) # remove the zip code from the location if it exists
 
-# get the pay range (only works if it is a pay range explicitly given by the company and it is in salary form, otherwise gives "Unknown")
-try:
-    pay_index = posting_dict['description'].find('$') # find the index of the first dollar sign (where pay range starts)
-    pay_string = posting_dict['description'][pay_index: pay_index+25] # get a string that includes the entire pay range
-    pay_separated = pay_string.split(' ')
-    if '.' in pay_separated[0]: # remove the decimal from the pay range if it exists
-        pay_separated[0] = pay_separated[0][:pay_separated[0].index('.')]
-    if '.' in pay_separated[2]: # remove the decimal from the pay range if it exists
-        pay_separated[2] = pay_separated[2][:pay_separated[2].index('.')]
-    pay = pay_separated[0][1:] + '-' + pay_separated[2][1:]
-except:
-    pay = "Unknown"
+    # get the pay range (only works if it is a pay range explicitly given by the company and it is in salary form, otherwise gives "Unknown")
+    try:
+        pay_index = posting_dict['description'].find('$') # find the index of the first dollar sign (where pay range starts)
+        pay_string = posting_dict['description'][pay_index: pay_index+25] # get a string that includes the entire pay range
+        pay_separated = pay_string.split(' ')
+        if '.' in pay_separated[0]: # remove the decimal from the pay range if it exists
+            pay_separated[0] = pay_separated[0][:pay_separated[0].index('.')]
+        if '.' in pay_separated[2]: # remove the decimal from the pay range if it exists
+            pay_separated[2] = pay_separated[2][:pay_separated[2].index('.')]
+        pay = pay_separated[0][1:] + '-' + pay_separated[2][1:]
+    except:
+        pay = "Unknown"
 
-wb = load_workbook(WB_NAME) # load the excel spreadsheet (must be in the same directory as the script)
-page = wb.active
-now = datetime.datetime.now()
-page.append([job_title, company, location, pay, str(now.month) + '/' + str(now.day), job_url]) # add the job info to the excel spreadsheet
-wb.save(filename=WB_NAME)
+    enter_info([job_title, company, location, pay, job_url]) # call the function to enter the info into the excel spreadsheet
+
+    
+# enters the info into the excel spreadsheet, must pass a list with the job title, company, location, pay, and url
+def enter_info(info):
+    wb = load_workbook(WB_NAME) # load the excel spreadsheet (must be in the same directory as the script)
+    page = wb.active
+    now = datetime.datetime.now()
+    page.append([info[0], info[1], info[2], info[3], str(now.month) + '/' + str(now.day), info[4]]) # add the job info to the excel spreadsheet
+    wb.save(filename=WB_NAME)
+
+if __name__ == "__main__":
+    main()
