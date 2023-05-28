17cccc82b695240ca2469a4da4b2532856ebedf1
andrewlemay <56843864+andrewlemay@users.noreply.github.com>
2023-05-28 14:57:46 -0400

    Complete TAPP3

--- TAPP3_TextBasedTicTacToe.py
+++ TAPP3_TextBasedTicTacToe.py
@@ -1,47 +1,93 @@
+## Fully working, but may return to make method/style improvements
+def main():
+    playAgain = 'y'
+    while playAgain.lower() == "y":
+        board = createBoard()
+        players = ['X', 'O']
+        validMoves = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+        turn = 0
+        player = players[0]
+        gameOver = False
+        printBoard(board)
+        while not gameOver:
+            move = input(f"Player {player} input the number for your move: ")
+            while move not in validMoves:
+                move = input(f"That is not a valid move, Player {player} please input a valid move: ")
+            validMoves.remove(move)
+            board = makeMove(board, player, move)
+            turn += 1
+            print()
+            printBoard(board)
+            gameOver = isGameOver(board, turn)
+            player = players[turn%2]
+        print(gameOver)
+        playAgain = input("Would you like to play again? (y/n): ")
+        print()
+
 def createBoard():
-    return [[" ", 1, " ", "|", " ", 2, " ", "|", " ", 3, " "],
-        ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-        [" ", 4, " ", "|", " ", 5, " ", "|", " ", 6, " "],
-        ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-        [" ", 7, " ", "|", " ", 8, " ", "|", " ", 9, " "]]
+    return [[' ', '1', ' ', '|', ' ', '2', ' ', '|', ' ', '3', ' '],
+        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+        [' ', '4', ' ', '|', ' ', '5', ' ', '|', ' ', '6', ' '],
+        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+        [' ', '7', ' ', '|', ' ', '8', ' ', '|', ' ', '9', ' ']]
 
-playAgain = True
-while playAgain:
-    board = createBoard()
+def printBoard(board):
+    for i in board:
+        print("".join(i))
 
-    print("Here is the board:")
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            print(board[i][j], end="")
-        print("\n")
-    print("The player playing X starts.")
-    player = 'X'
-    
-    end = "z"
-    while end == "":
+def isGameOver(board, turn):
+    if turn <= 4:
+        return False
+    elif board[0][1] == board[0][5] == board[0][9]:
+        return f"{board[0][1]} wins!"
+    elif board[2][1] == board[2][5] == board[2][9]:
+        return f"{board[2][1]} wins!"
+    elif board[4][1] == board[4][5] == board[4][9]:
+        return f"{board[4][1]} wins!"
+    elif board[0][1] == board[2][1] == board[4][1]:
+        return f"{board[0][1]} wins!"
+    elif board[0][5] == board[2][5] == board[4][5]:
+        return f"{board[0][5]} wins!"
+    elif board[0][9] == board[2][9] == board[4][9]:
+        return f"{board[0][9]} wins!"
+    elif board[0][1] == board[2][5] == board[4][9]:
+        return f"{board[0][1]} wins!"
+    elif board[0][9] == board[2][5] == board[4][1]:
+        return f"{board[0][9]} wins!"
+    elif turn == 9:
+        return "The game is a draw"
+    else:
+        return False
 
-        invalidMove = True
-        while(invalidMove):
-            move = input(f"Player \"{player}\" please type in the number of the space for your move.")
-            for i in range(len(board[0])):
-                break
+def makeMove(board, player, move):
+    match move:
+        case '1':
+            board[0][1] = player
+            return board
+        case '2':
+            board[0][5] = player
+            return board
+        case '3':
+            board[0][9] = player
+            return board
+        case '4':
+            board[2][1] = player
+            return board
+        case '5':
+            board[2][5] = player
+            return board
+        case '6':
+            board[2][9] = player
+            return board
+        case '7':
+            board[4][1] = player
+            return board
+        case '8':
+            board[4][5] = player
+            return board
+        case '9':
+            board[4][9] = player
+            return board
 
-        if not invalidMove:
-            if board[0][1] == board[0][5] == board[0][9]:
-                end = board[0][1]
-            elif board[2][1] == board[2][5] == board[2][9]:
-                end = board[2][1]
-            elif board[4][1] == board[4][5] == board[4][9]:
-                end = board[4][1]
-            elif board[0][1] == board[2][1] == board[4][1]:
-                end = board[0][1]
-            elif board[0][5] == board[2][5] == board[4][5]:
-                end = board[0][5]
-            elif board[0][9] == board[2][9] == board[4][9]:
-                end = board[0][9]
-            elif board[0][1] == board[2][5] == board[4][9]:
-                end = board[0][1]
-            elif board[4][1] == board[2][5] == board[0][9]:
-                end = board[4][1]
-
-    playAgain = False
+if __name__ == "__main__":
+    main()
